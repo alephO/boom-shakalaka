3fb988bf024be4862f050dacd90b261672d2ebe5
JackZhongTJ <zhongtianjie2014@gmail.com>
2015-10-17 17:38:48 -0400

    Fill information interface

--- Client/PocketGuide/app/src/main/java/com/brandeis/zhongzhongzhong/pocketguide/Fill_Information.java
+++ Client/PocketGuide/app/src/main/java/com/brandeis/zhongzhongzhong/pocketguide/Fill_Information.java
@@ -4,11 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.util.Log;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
 
 public class Fill_Information extends AppCompatActivity {
-
+    private static final String TAG = "test";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +24,30 @@ public class Fill_Information extends AppCompatActivity {
         TextView location_title = (TextView) findViewById(R.id.fillinformation_location_title);
         location_title.setText(location_name);
         //Log.d("blabla", location_name);
+
+        Spinner spinner = (Spinner) findViewById(R.id.fillinformation_location_requirements_dropdown);
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(parent.getContext(),
+                        "OnItemSelectedListener:" + parent.getItemAtPosition(position).toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Button confirmbtn = (Button) findViewById(R.id.fillinformation_confirmbtn);
+
+        View.OnClickListener clkconfirmbtn = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                  Log.d(TAG,"test");
+            }
+        };
+        confirmbtn.setOnClickListener(clkconfirmbtn);
     }
 }
