828f2492c4af911bddf5068e1b68188904c52a5f
JackZhongTJ <zhongtianjie2014@gmail.com>
2015-10-18 13:56:13 -0400

    added calendar view

--- Client/PocketGuide/app/src/main/java/com/brandeis/zhongzhongzhong/pocketguide/Fill_Information.java
+++ Client/PocketGuide/app/src/main/java/com/brandeis/zhongzhongzhong/pocketguide/Fill_Information.java
@@ -1,18 +1,27 @@
 package com.brandeis.zhongzhongzhong.pocketguide;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.util.Log;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 import android.content.Intent;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 
 public class Fill_Information extends AppCompatActivity {
     private static final String TAG = "test";
@@ -25,6 +34,60 @@ public class Fill_Information extends AppCompatActivity {
         TextView location_title = (TextView) findViewById(R.id.fillinformation_location_title);
         location_title.setText(location_name);
         //Log.d("blabla", location_name);
+
+        final SimpleDateFormat dateFormatter;
+        dateFormatter = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+        final EditText fromdate = (EditText) findViewById(R.id.fillinformation_fromdate_editText);
+        final EditText todate = (EditText) findViewById(R.id.fillinformation_todate_editText);
+        fromdate.setInputType(InputType.TYPE_NULL);
+        fromdate.requestFocus();
+        todate.setInputType(InputType.TYPE_NULL);
+
+        Calendar newCalendar = Calendar.getInstance();
+        final DatePickerDialog fromDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                fromdate.setText(dateFormatter.format(newDate.getTime()));
+            }
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH)
+        );
+
+        final DatePickerDialog toDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                todate.setText(dateFormatter.format(newDate.getTime()));
+            }
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH)
+        );
+
+
+       /* public boolean onCreateOptionsMenu(Menu menu){
+            getMenuInflater().inflate(R.menu.main, menu);
+            return true;
+        }*/
+
+        View.OnClickListener fromDateCLK = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fromDatePickerDialog.show();
+            }
+        };
+
+        View.OnClickListener toDateCLK = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toDatePickerDialog.show();
+            }
+        };
+
+        fromdate.setOnClickListener(fromDateCLK);
+        todate.setOnClickListener(toDateCLK);
+
+
 
         Spinner spinner = (Spinner) findViewById(R.id.fillinformation_location_requirements_dropdown);
         spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
