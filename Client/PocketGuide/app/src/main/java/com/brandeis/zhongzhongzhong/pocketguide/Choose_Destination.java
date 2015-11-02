0f07e54895ba6f5320af80a8ce055e299bd5c9a6
JackZhongTJ <zhongtianjie2014@gmail.com>
2015-11-01 22:39:09 -0500

    sqlite initial commit

--- Client/PocketGuide/app/src/main/java/com/brandeis/zhongzhongzhong/pocketguide/Choose_Destination.java
+++ Client/PocketGuide/app/src/main/java/com/brandeis/zhongzhongzhong/pocketguide/Choose_Destination.java
@@ -3,6 +3,7 @@ package com.brandeis.zhongzhongzhong.pocketguide;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,10 +15,14 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 
+import java.util.ArrayList;
+
 public class Choose_Destination extends AppCompatActivity {
 
     /* location list should be retrieved from database, but now just use string stored in the resource file*/
     public String [] location;
+    public static final String TAG = "create?";
+    DBHelper mydb;
 
 
     @Override
@@ -26,10 +31,12 @@ public class Choose_Destination extends AppCompatActivity {
         /*hide title bar*/
         //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_choose_destination);
-        location = getResources().getStringArray(R.array.location_list);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, location);
-
+        //location = getResources().getStringArray(R.array.location_list);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, location);
+        mydb = new DBHelper(this);
+        ArrayList array_list = mydb.getAllNations();
+        //Log.d(TAG, array_list.get(1));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array_list);
         AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.Chooselocation_autoCompleteTextView);
 
         autoCompleteTextView.setThreshold(1);
---
+++ Client/PocketGuide/app/src/main/java/com/brandeis/zhongzhongzhong/pocketguide/DBHelper.java
@@ -0,0 +1,69 @@
+package com.brandeis.zhongzhongzhong.pocketguide;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper{
+    public static final String DATABASE_NAME = "boom.db";
+    public static final String NATION_TABLE_NAME = "nation";
+    public static final String NATION_COLUMN_ID = "nation_id";
+    public static final String NATION_COLUMN_NAME = "nation_name";
+    public static final String TAG = "create?";
+
+    public DBHelper(Context context){
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(
+                "create table nation" +
+                        "(nation_id integer primary key autoincrement, nation_name text)"
+        );
+        Log.d(TAG, "onCreate ");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS nation");
+        onCreate(db);
+    }
+
+    public Cursor getNation(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select nation_name from nation",null);
+        return res;
+    }
+    public int numberOfNations(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, NATION_TABLE_NAME);
+        return numRows;
+    }
+
+    public ArrayList<String> getAllNations(){
+        ArrayList<String> array_list = new ArrayList<String>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        //Cursor res = this.getNation();
+        Cursor res = db.rawQuery("select nation_name from nation", null);
+        res.moveToFirst();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, NATION_TABLE_NAME);
+        Log.d(TAG, "nation count: " + numRows);
+
+        while(res.isAfterLast() == false){
+            array_list.add(res.getString(res.getColumnIndex(NATION_COLUMN_NAME)));
+            Log.d(TAG, "Value: " + res.getString(res.getColumnIndex(NATION_COLUMN_NAME)));
+            res.moveToNext();
+        }
+        Log.d(TAG, "get all nations ");
+        return array_list;
+    }
+
+}
