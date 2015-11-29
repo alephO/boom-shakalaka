b75b410a139f8362571875fbd36473773b5050c9
JackZhongTJ <zhongtianjie2014@gmail.com>
2015-11-28 23:09:37 -0500

    added drag and drop sort listed view

--- Client/PocketGuide/app/src/main/java/com/brandeis/zhongzhongzhong/pocketguide/Fill_Information.java
+++ Client/PocketGuide/app/src/main/java/com/brandeis/zhongzhongzhong/pocketguide/Fill_Information.java
@@ -11,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.util.Log;
@@ -19,6 +20,7 @@ import android.widget.Toast;
 import android.content.Intent;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -26,13 +28,18 @@ import java.util.Locale;
 public class Fill_Information extends AppCompatActivity {
     private static final String TAG = "test";
     //final EditText et = (EditText) findViewById(R.id.fillinformation_note);
+    public String[] requirements;
+    static String fromfromdate;
+    static String totodate;
+    static String notenote;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_information);
 
-        String location_name = getIntent().getStringExtra("location_name");
+        final String location_name = getIntent().getStringExtra("location_name");
         TextView location_title = (TextView) findViewById(R.id.fillinformation_location_title);
         location_title.setText(location_name);
         //Log.d("blabla", location_name);
@@ -52,6 +59,7 @@ public class Fill_Information extends AppCompatActivity {
                 Calendar newDate = Calendar.getInstance();
                 newDate.set(year, monthOfYear, dayOfMonth);
                 fromdate.setText(dateFormatter.format(newDate.getTime()));
+                fromfromdate = dateFormatter.format(newDate.getTime());
             }
         }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH)
         );
@@ -62,6 +70,7 @@ public class Fill_Information extends AppCompatActivity {
                 Calendar newDate = Calendar.getInstance();
                 newDate.set(year, monthOfYear, dayOfMonth);
                 todate.setText(dateFormatter.format(newDate.getTime()));
+                totodate = dateFormatter.format(newDate.getTime());
             }
         }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH)
         );
@@ -90,7 +99,7 @@ public class Fill_Information extends AppCompatActivity {
         todate.setOnClickListener(toDateCLK);
 
 
-
+/*
         Spinner spinner = (Spinner) findViewById(R.id.fillinformation_location_requirements_dropdown);
         spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
             @Override
@@ -105,11 +114,28 @@ public class Fill_Information extends AppCompatActivity {
 
             }
         });
+*/
+        /* requirement sort */
+        requirements = getResources().getStringArray(R.array.requirements_array);
 
+        ArrayList<String> requirementsList = new ArrayList<String>();
+        for (int i = 0; i < requirements.length; ++i) {
+            requirementsList.add(requirements[i]);
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(this, R.layout.text_view, requirementsList);
+        DynamicListView listView = (DynamicListView) findViewById(R.id.listview);
 
+        listView.setCheeseList(requirementsList);
+        listView.setAdapter(adapter);
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
 
         Button confirmbtn = (Button) findViewById(R.id.fillinformation_confirmbtn);
+
+
+        EditText notes = (EditText) findViewById(R.id.fillinformation_note);
+        notenote = notes.getText().toString();
+
 
         View.OnClickListener clkconfirmbtn = new View.OnClickListener() {
             @Override
@@ -119,6 +145,14 @@ public class Fill_Information extends AppCompatActivity {
                 This is where all variables should be encoded and sent to server.
 
                  */
+                Log.d("Destination", location_name);
+                Log.d("fromdate",fromfromdate);
+                Log.d("todate",totodate);
+                Log.d("first choice", adapter.getItem(0));
+                Log.d("second choice", adapter.getItem(1));
+                Log.d("third choice", adapter.getItem(2));
+                Log.d("fourth choice", adapter.getItem(3));
+                Log.d("notes",notenote);
 
                 Intent intent = new Intent(Fill_Information.this, Confirmation_Page.class);
                 startActivity(intent);
